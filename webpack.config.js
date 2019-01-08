bd97e484db4405faa20dcaee8cabc3eca4b06868
Adrian Liechti <jahweh@jahweh.ch>
2019-01-08 20:34:16 +0100

    configure webpack

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+let config = {
+    entry: './src/index.ts',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js']
+    },
+    output: {
+        filename: 'script.js',
+        path: path.resolve(__dirname, 'web/build')
+    }
+};
+
+module.exports = (env, argv) => {
+    if (argv.mode === 'development') {
+        config.devtool = 'inline-source-map';
+    }
+
+    return config;
+};
