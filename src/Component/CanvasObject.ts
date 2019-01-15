4c7e169bb7e7268b2a53089dc1bd9e431c203891
Adrian Liechti <jahweh@jahweh.ch>
2019-01-15 14:09:00 +0100

    replace custom canvas with pixijs

--- src/Component/CanvasObject.ts
+++
@@ -1,9 +0,0 @@
-export abstract class CanvasObject<Props = undefined> {
-    protected props: Props;
-
-    public constructor(props: Props) {
-        this.props = props;
-    }
-
-    public abstract renderTo(context: CanvasRenderingContext2D): void;
-}
