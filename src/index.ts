4d9f3e4d8e68fd1ceab0a3f85d958404eaa661b7
Adrian Liechti <jahweh@jahweh.ch>
2019-01-19 19:57:19 +0100

    wip: hexagon grid

--- src/index.ts
+++ src/index.ts
@@ -7,21 +7,21 @@ window.addEventListener('resize', () => {
     app.renderer.resize(window.innerWidth, window.innerHeight);
 });
 
-const width = 10;
-const height = 10;
-const size = 20;
-const sizeIncrementX = 3;
-const sizeIncrementY = 3;
-for (let x = 0; x < width; x++) {
-    for (let y = 0; y < height; y++) {
-        const isEven = y % 2;
-        const hexagon = new Hexagon(size, 2);
-        hexagon.x = size * sizeIncrementX * x;
-        hexagon.y = size * sizeIncrementY * y;
-        // hexagon.pivot.x = size * sizeIncrementX / 2;
-        // hexagon.pivot.y = size * sizeIncrementY / 2;
+const radius = 25;
+const hexWidth = 2 * radius;
+const hexHeight = Math.sqrt(3) * radius;
+const width = Math.floor((window.innerWidth - hexWidth / 4) / (hexWidth * 3 / 4));
+const height = Math.floor((window.innerHeight - hexHeight * 3 / 4) / hexHeight);
+for (let y = 0; y < height; y++) {
+    for (let x = 0; x < width; x++) {
+        const isEven = x % 2;
+        const hexagon = new Hexagon(radius, 2);
+        hexagon.x = hexWidth * x * 3 / 4;
+        hexagon.y = hexHeight * y;
+        // hexagon.pivot.x = radius * sizeIncrementX / 2;
+        // hexagon.pivot.y = radius * sizeIncrementY / 2;
         if (isEven) {
-            // hexagon.x += size * 1.5;
+            hexagon.y += hexHeight / 2;
             // hexagon.rotation = Math.PI * 2 * 0.125;
         }
         app.stage.addChild(hexagon);
