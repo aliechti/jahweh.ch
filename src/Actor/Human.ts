0f09d6842db0448af22cf858bc937b6839fd20c6
Adrian Liechti <jahweh@jahweh.ch>
2019-03-24 08:58:15 +0100

    refactor: get player fields in human actor for separate use

--- src/Actor/Human.ts
+++ src/Actor/Human.ts
@@ -1,18 +1,23 @@
+import {HexagonField} from '../Component/HexagonField';
 import {Unit} from '../Component/Unit';
 import {Actor, OnTurnProps, Player} from '../Manager/PlayerManager';
 
 export class Human implements Actor {
     public onTurnStart = (props: OnTurnProps) => {
-        const units = this.getPlayerUnits(props.player);
         // Set player unit interactivity
+        const fields = this.getPlayerFields(props.player);
+        const units = this.getFieldUnits(fields);
         for (const unit of units) {
-            unit.interactive = true;
-            unit.buttonMode = true;
+            if (unit.canMove) {
+                unit.interactive = true;
+                unit.buttonMode = true;
+            }
         }
     };
 
     public onTurnEnd = (props: OnTurnProps) => {
-        const units = this.getPlayerUnits(props.player);
+        const fields = this.getPlayerFields(props.player);
+        const units = this.getFieldUnits(fields);
         // Remove player unit interactivity
         for (const unit of units) {
             unit.interactive = false;
@@ -20,13 +25,17 @@ export class Human implements Actor {
         }
     };
 
-    private getPlayerUnits(player: Player): Unit[] {
+    private getFieldUnits(fields: HexagonField[]): Unit[] {
+        return fields.map((field) => {
+            return field.unit;
+        }).filter((unit) => unit !== undefined) as Unit[];
+    }
+
+    private getPlayerFields(player: Player): HexagonField[] {
         return player.territories.map(
             (territory) => territory.props.fields,
         ).reduce((previous, current) => {
             return Array().concat(previous, current);
-        }).map((field) => {
-            return field.unit;
-        }).filter((unit) => unit !== undefined) as Unit[];
+        });
     }
 }
