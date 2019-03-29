16c5337a78f776273d45c2449ae4001d67da965b
Adrian Liechti <jahweh@jahweh.ch>
2019-03-29 12:14:50 +0100

    feat: SimpleAI merge units method (not used yet)
    This is not yet used because there is a defending mechanism needed,
    otherwise the AI just overcommits on strong units and doesn't have
    enough weak units left to defend

--- src/Actor/SimpleAI.ts
+++ src/Actor/SimpleAI.ts
@@ -1,4 +1,5 @@
 import {Game} from '../Component/Game';
+import {Territory} from '../Component/Territory';
 import {Unit} from '../Component/Unit';
 import {Actor} from '../Manager/PlayerManager';
 
@@ -16,39 +17,14 @@ export class SimpleAI implements Actor {
                 return field.unit;
             }).filter((unit) => unit !== undefined) as Unit[];
             if (units.length > 0) {
-                const territoryNeighbors = map.getTerritoryNeighbors(territory);
-                const strengthNeighbors = territoryNeighbors.map((neighbor) => {
-                    return {
-                        field: neighbor,
-                        strength: movementManager.getFieldDefendingStrength(neighbor),
-                    };
-                });
-                const requiredStrength = Math.min(...strengthNeighbors.map((neighbor) => neighbor.strength));
-                // Filter too weak units
-                units = units.filter((unit) => {
-                    return unit.props.type.strength > requiredStrength;
-                });
-                // Order units by strength to not waste any strong unit
-                units.sort((a, b) => {
-                    return a.props.type.strength - b.props.type.strength;
-                });
-                for (const unit of units) {
-                    if (!unit.canMove) {
-                        continue;
-                    }
-                    for (const neighbor of strengthNeighbors) {
-                        if (unit.props.type.strength > neighbor.strength
-                            && movementManager.move(unit, neighbor.field, player)
-                        ) {
-                            // Remove neighbor, to not check it again
-                            const index = strengthNeighbors.indexOf(neighbor);
-                            if (index !== -1) {
-                                strengthNeighbors.splice(index, 1);
-                            }
-                            break;
-                        }
-                    }
-                }
+                this.moveUnits({units, territory, game});
+                // Merge not moved units
+                // todo: this is not yet used because there is a defending mechanism needed,
+                //  otherwise the AI just overcommits on strong units and doesn't have enough weak units left to defend
+                // const mergedUnits = this.mergeUnits({units, territory, game});
+                // if (mergedUnits.length > 0) {
+                //     this.moveUnits({units: mergedUnits, territory, game});
+                // }
             }
             console.timeEnd('move-units ' + game.player.id);
             console.time('buy-units ' + game.player.id);
@@ -79,4 +55,102 @@ export class SimpleAI implements Actor {
             console.timeEnd('buy-units ' + game.player.id);
         }
     };
+
+    private moveUnits = (
+        {units, territory, game}: {
+            units: Unit[],
+            territory: Territory,
+            game: Game,
+        },
+    ) => {
+        const {map, movementManager, player} = game;
+        const territoryNeighbors = map.getTerritoryNeighbors(territory);
+        const strengthNeighbors = territoryNeighbors.map((neighbor) => {
+            return {
+                field: neighbor,
+                strength: movementManager.getFieldDefendingStrength(neighbor),
+            };
+        });
+        const requiredStrength = Math.min(...strengthNeighbors.map((neighbor) => neighbor.strength));
+        // Filter too weak units
+        units = units.filter((unit) => {
+            return unit.props.type.strength > requiredStrength;
+        });
+        // Order units by strength to not waste any strong unit
+        units.sort((a, b) => {
+            return a.props.type.strength - b.props.type.strength;
+        });
+        for (const unit of units) {
+            if (!unit.canMove) {
+                continue;
+            }
+            for (const neighbor of strengthNeighbors) {
+                if (unit.props.type.strength > neighbor.strength
+                    && movementManager.move(unit, neighbor.field, player)
+                ) {
+                    // Remove neighbor, to not check it again
+                    const index = strengthNeighbors.indexOf(neighbor);
+                    if (index !== -1) {
+                        strengthNeighbors.splice(index, 1);
+                    }
+                    break;
+                }
+            }
+        }
+    };
+
+    private mergeUnits = (
+        {units, territory, game}: {
+            units: Unit[],
+            territory: Territory,
+            game: Game,
+        },
+    ): Unit[] => {
+        const {map, movementManager} = game;
+        const territoryNeighbors = map.getTerritoryNeighbors(territory);
+        // Get and order territories strengths
+        const requiredStrengths = territoryNeighbors.map((neighbor) => {
+            return movementManager.getFieldDefendingStrength(neighbor);
+        }).sort((a, b) => a - b);
+        // Filter units which can move and order them by strength
+        units = units.filter((unit) => unit.canMove).sort((a, b) => {
+            return a.props.type.strength - b.props.type.strength;
+        });
+        let requiredStrength = requiredStrengths.shift();
+        const mergedUnits: Unit[] = [];
+        const removedUnits: Unit[] = [];
+        for (const unit of units) {
+            // Cancel if there isn't any field to attack
+            if (requiredStrength === undefined) {
+                break;
+            }
+            // Continue if this unit got merged
+            if (removedUnits.includes(unit)) {
+                continue;
+            }
+            // Find a unit which can be merged to attack a neighbor
+            const nextUnit = units.find((u) => {
+                const type = u.props.type;
+                const strength = type.strength + unit.props.type.strength;
+                if (strength <= (requiredStrength as number)) {
+                    return false;
+                }
+                const mergedType = movementManager.getMergedType(type, unit.props.type);
+                if (mergedType === undefined) {
+                    return false;
+                }
+                // Prevent bankruptcy by not merge the unit if the salaries are bigger than the income
+                return mergedType.salary + territory.salaries() < territory.income();
+            });
+            // Cancel if no unit was found (because of the sort order the next ones are too costly)
+            if (nextUnit === undefined) {
+                break;
+            }
+            if (movementManager.mergeUnits(unit, nextUnit)) {
+                mergedUnits.push(unit);
+                removedUnits.push(nextUnit);
+            }
+        }
+        return mergedUnits;
+    }
 }
