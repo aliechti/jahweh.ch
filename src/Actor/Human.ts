e4ca465342e2fa05518b7cc74664e4b17a5c0b15
Adrian Liechti <jahweh@jahweh.ch>
2019-04-01 16:47:37 +0200

    feat: outline selected territory

--- src/Actor/Human.ts
+++ src/Actor/Human.ts
@@ -1,16 +1,21 @@
 import {HexagonField} from '../Component/HexagonField';
 import {Territory} from '../Component/Territory';
 import {Unit, UnitType} from '../Component/Unit';
+import {axialAdd, AxialCoordinates, axialDirections} from '../Function/Coordinates';
 import {Actor, ActorProps, Player} from '../Manager/PlayerManager';
+import Container = PIXI.Container;
+import Graphics = PIXI.Graphics;
 import InteractionEvent = PIXI.interaction.InteractionEvent;
+import Polygon = PIXI.Polygon;
 
 type InteractionHandler = (e: InteractionEvent) => void;
 
 export class Human implements Actor {
 
-    public selectedTerritory?: Territory;
     private props: ActorProps;
     private fieldClickHandlers: Map<HexagonField, InteractionHandler> = new Map();
+    private selectedTerritory?: Territory;
+    private outlineContainer: Container = new Container();
 
     public init = (props: ActorProps) => {
         this.props = props;
@@ -33,6 +38,8 @@ export class Human implements Actor {
                 unit.setInteractive(true);
             }
         }
+        // Attach outline Container
+        this.props.game.map.grid.addChild(this.outlineContainer);
         // Update Panel
         this.props.updatePanel({});
     };
@@ -50,6 +57,8 @@ export class Human implements Actor {
             field.off('click', handler);
             this.fieldClickHandlers.delete(field);
         }
+        // Remove outline Container
+        this.props.game.map.grid.removeChild(this.outlineContainer);
     };
 
     public onPanelUnitClick = (type: UnitType, position: { x: number, y: number }) => {
@@ -121,10 +130,10 @@ export class Human implements Actor {
 
     private selectTerritory = (territory?: Territory) => {
         if (this.selectedTerritory) {
-            this.tintTerritory(this.selectedTerritory, 0xffffff);
+            this.outlineContainer.removeChildren();
         }
         if (territory) {
-            this.tintTerritory(territory, 0xaaaaaa);
+            this.outlineTerritory(territory, 0x000000);
         }
         this.selectedTerritory = territory;
         this.props.updatePanel({territory});
@@ -134,5 +143,38 @@ export class Human implements Actor {
         for (const field of territory.props.fields) {
             field.tint = tint;
         }
+    }
+
+    private outlineTerritory(territory: Territory, color: number) {
+        const {grid} = this.props.game.map;
+        for (const field of territory.props.fields) {
+            this.outlineField(
+                field,
+                axialDirections.filter((direction) => {
+                    const neighbor = grid.get(axialAdd(field, direction));
+                    return !neighbor || neighbor.player !== this.props.player;
+                }),
+                color,
+            );
+        }
+    }
+
+    private outlineField(field: HexagonField, directions: AxialCoordinates[], color: number) {
+        const points = (field.hitArea as Polygon).points;
+        const graphics = new Graphics();
+        graphics.position = field.position;
+        graphics.lineStyle(4, color);
+        const getPoint = (i: number) => {
+            return points[i % 12];
+        };
+        for (const direction of directions) {
+            const index = axialDirections.findIndex((d) => d.q === direction.q && d.r === direction.r);
+            if (index !== -1) {
+                let i = (index + 4) * 2;
+                graphics.moveTo(getPoint(i++), getPoint(i++));
+                graphics.lineTo(getPoint(i++), getPoint(i++));
+            }
+        }
+        this.outlineContainer.addChild(graphics);
     }
 }
