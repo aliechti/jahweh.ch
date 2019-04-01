6ca9e8c47d12ac41bef9b8ae44e968c4205642dc
Adrian Liechti <jahweh@jahweh.ch>
2019-04-01 16:44:57 +0200

    feat: additional color functions

--- src/Function/Color.ts
+++ src/Function/Color.ts
@@ -3,3 +3,23 @@ export function colorToString(color: number): string {
     const padding = '0'.repeat(6 - hex.length);
     return '#' + padding + hex;
 }
+
+export function colorSplit(color: number) {
+    const r = (color & 0xff0000) >> 16;
+    const g = (color & 0x00ff00) >> 8;
+    const b = (color & 0x0000ff);
+    return {r, g, b};
+}
+
+export function colorConcat(color: { r: number, g: number, b: number }) {
+    return (color.r << 16) + (color.g << 8) + color.b;
+}
+
+export function colorMultiply(color: number, multiplier: number) {
+    const {r, g, b} = colorSplit(color);
+    return colorConcat({
+        r: Math.min(Math.max(r * multiplier, 0), 255),
+        g: Math.min(Math.max(g * multiplier, 0), 255),
+        b: Math.min(Math.max(b * multiplier, 0), 255),
+    });
+}
