258bcbc6200c48472b3e74ec43536712a7035e41
Adrian Liechti <jahweh@jahweh.ch>
2019-04-08 14:47:49 +0200

    refactor: outsource OptionsSelector into own component

---
+++ src/Component/Overlay/OptionsSelector.tsx
@@ -0,0 +1,56 @@
+import * as React from 'react';
+import {GameOptions, PlayerPicker, Shape} from '../../Function/GameFactory';
+
+interface Props {
+    options: GameOptions;
+    onSetOptions: (options: GameOptions) => void;
+}
+
+const shapes: Shape[] = ['rectangle', 'rhombus', 'ring', 'hexagon', 'load'];
+const playerPickers: PlayerPicker[] = ['random', 'even'];
+
+export class OptionsSelector extends React.Component<Props> {
+
+    private handleSetOption = (name: keyof GameOptions, value: any) => {
+        const {options, onSetOptions} = this.props;
+        options[name] = value;
+        onSetOptions(options);
+    };
+
+    render() {
+        const {options} = this.props;
+        return (
+            <>
+                <label>Shape</label>
+                <select value={options.shape}
+                        onChange={(e) => this.handleSetOption('shape', e.target.value)}>
+                    {shapes.map((shape) => {
+                        return <option key={shape} value={shape}>{shape}</option>;
+                    })}
+                </select>
+                <label>Player picker</label>
+                <select value={options.playerPicker}
+                        onChange={(e) => this.handleSetOption('playerPicker', e.target.value)}>
+                    {playerPickers.map((chooser) => {
+                        return <option key={chooser} value={chooser}>{chooser}</option>;
+                    })}
+                </select>
+                {options.shape === 'load' ? '' :
+                    options.shape === 'hexagon' || options.shape === 'ring'
+                        ? <>
+                            <label>Radius</label>
+                            <input value={options.radius}
+                                   onChange={(e) => this.handleSetOption('radius', Number(e.target.value))}/>
+                        </> : <>
+                            <label>Columns</label>
+                            <input value={options.columns}
+                                   onChange={(e) => this.handleSetOption('columns', Number(e.target.value))}/>
+                            <label>Rows</label>
+                            <input value={options.rows}
+                                   onChange={(e) => this.handleSetOption('rows', Number(e.target.value))}/>
+                        </>
+                }
+            </>
+        );
+    }
+}
--- src/Component/Overlay/Start.tsx
+++ src/Component/Overlay/Start.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
-import {GameOptions, PlayerPicker, Shape} from '../../Function/GameFactory';
+import {GameOptions} from '../../Function/GameFactory';
+import {OptionsSelector} from './OptionsSelector';
 
 interface Props {
     options: GameOptions;
@@ -10,52 +11,14 @@ interface Props {
     onSetOptions: (options: GameOptions) => void;
 }
 
-const shapes: Shape[] = ['rectangle', 'rhombus', 'ring', 'hexagon', 'load'];
-const playerPickers: PlayerPicker[] = ['random', 'even'];
-
 export class Start extends React.Component<Props> {
-
-    private handleSetOption = (name: keyof GameOptions, value: any) => {
-        const {options, onSetOptions} = this.props;
-        options[name] = value;
-        onSetOptions(options);
-    };
-
     render() {
-        const {options, canResume, onClickStart, onClickResume, onClickReadme} = this.props;
+        const {options, canResume, onClickStart, onClickResume, onClickReadme, onSetOptions} = this.props;
         return (
             <div className="full background-dim row">
                 <div className="center">
                     <div style={{width: '10rem'}}>
-                        <label>Shape</label>
-                        <select value={options.shape}
-                                onChange={(e) => this.handleSetOption('shape', e.target.value)}>
-                            {shapes.map((shape) => {
-                                return <option key={shape} value={shape}>{shape}</option>;
-                            })}
-                        </select>
-                        <label>Player picker</label>
-                        <select value={options.playerPicker}
-                                onChange={(e) => this.handleSetOption('playerPicker', e.target.value)}>
-                            {playerPickers.map((chooser) => {
-                                return <option key={chooser} value={chooser}>{chooser}</option>;
-                            })}
-                        </select>
-                        {options.shape === 'load' ? '' :
-                            options.shape === 'hexagon' || options.shape === 'ring'
-                                ? <>
-                                    <label>Radius</label>
-                                    <input value={options.radius}
-                                           onChange={(e) => this.handleSetOption('radius', Number(e.target.value))}/>
-                                </> : <>
-                                    <label>Columns</label>
-                                    <input value={options.columns}
-                                           onChange={(e) => this.handleSetOption('columns', Number(e.target.value))}/>
-                                    <label>Rows</label>
-                                    <input value={options.rows}
-                                           onChange={(e) => this.handleSetOption('rows', Number(e.target.value))}/>
-                                </>
-                        }
+                        <OptionsSelector options={options} onSetOptions={onSetOptions}/>
                         <div className="row" style={{marginTop: '0.5rem'}}>
                             <div className="col" style={{textAlign: 'center', marginTop: '0.5rem'}}>
                                 <button type="button" onClick={onClickStart}>Start</button>
